c265085c6b19fa9e44329d0788f41a409aa0b526
otto <otto@g.mgzl.jp>
2017-11-09 17:32:00 +0900

    eslintを追加

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    'max-len': ["off"],
+    'spaced-comment': 'warn',
+    'curly': ['error', 'all'],
+    'no-plusplus': ['error', {'allowForLoopAfterthoughts': true}],
+    'func-names': ['off'],
+    'guard-for-in': ['warn'],
+    'arrow-parens': ['warn', "as-needed"],
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'functions': 'never'
+    }],
+    //'no-comma-dangle': ['warn'],
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
